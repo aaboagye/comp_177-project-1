506ddc5cf9b360c304bb1fc9ef4c387aad83fb06
Aseda Gyeke Aboagye <aaboagye@lithium.(none)>
2012-10-10 12:40:52 -0700

    Made more changes and added the parlor struct.

--- server.c
+++ server.c
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include "server.h"
 
 #define QUEUE 10
 #define MAX_BUF_SIZE 1024
@@ -56,11 +57,28 @@ int main(int argc, char **argv){
 	 *	client's connect().
 	 */
 	char buffer[MAX_BUF_SIZE];
-	int bytes_read;
-	bytes_read = recv(sockfd_client, buffer, 4,0);
-	printf("\n\"%s\"\n",buffer);
-	/* I'm not entirely sure how many bytes I should be reading in.
-	 * 32 bits = 1 byte.
-	 */
+	memset(buffer, 0, MAX_BUF_SIZE);
+	/* read in transmission from client. */
+	/* Now I need to separate the data into the respective fields */
+	char vendor1[MAX_BUF_SIZE];
+	char vendor2[MAX_BUF_SIZE];
+	int total_size1 = sizeof(struct parlor) - 1 + strlen(vendor1) + 1;
+	int total_size2 = sizeof(struct parlor) - 1 + strlen(vendor2) + 1;
+	char* buffer_ptr1 = malloc(total_size1);
+	char* buffer_ptr2 = malloc(total_size2);
+	struct parlor* ptr_bytes1;
+	struct parlor* ptr_bytes2;
+	ptr_bytes1 = (struct parlor*) buffer_ptr1;
+	ptr_bytes2 = (struct parlor*) buffer_ptr2;
+	
+	/* here there needs to be something along the lines of: */
+	recv(sockfd_client, buffer, 2, 0); /* receiving 16 bits = 2 bytes */
+	ptr_bytes1->pizza_dollars = (uint16_t) buffer;
+	recv(sockfd_client, buffer, 2, 0);
+	ptr_bytes1->pizza_cents = (uint16_t) buffer;	
+	printf("$%i.%i\n",ptr_bytes1->pizza_dollars, ptr_bytes1->pizza_cents);
+	
+	close(sockfd);
+	freeaddrinfo(res);
 	return 0;
 }
